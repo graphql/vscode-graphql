8dbece86d4653b990f3b9faa4a2080faa7902714
Prismo <prismabots@gmail.com>
2020-03-12 15:33:34 +0530

    fix bash

--- publish.sh
+++ publish.sh
@@ -4,29 +4,28 @@ if [[ ! -f .envrc ]]; then
   export $(cat .envrc | xargs)
 fi
 
+echo "============================"
 echo "PAT: $PAT"
-echo "LOCAL: $LOCAL"
+echo "PRODUCTION: $PRODUCTION"
+echo "============================"
 
 if [[ -z "$PAT" ]]; then
     echo "\$PAT is empty. Please set the value of $PAT"
 elif [[ -n "$PAT" ]]; then
-    if [[ -z "$LOCAL" ]]; then
+    if [[ "$PRODUCTION" == "1" ]]; then
         ./node_modules/.bin/vsce publish patch --pat $PAT
-    elif [[ -n "$LOCAL" ]]; then
-        echo "Printing the command because LOCAL is set"
-        echo "npm run vsce:publish patch --pat $PAT"
+    else
+        echo "Printing the command because PRODUCTION is not set"
+        echo "./node_modules/.bin/vsce publish patch --pat $PAT"
     fi
 fi
 
-echo "PAT: $PAT"
-echo "LOCAL: $LOCAL"
-
-if [[ -z "$LOCAL" ]]; then
+if [[ "$PRODUCTION" == "1" ]]; then
     git config --global user.email "prismabots@gmail.com"
     git config --global user.name "Prismo"
     git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
     git pull github "${GITHUB_REF}" --ff-only
     git push github HEAD:"${GITHUB_REF}"
-elif [[ -n "$LOCAL" ]]; then
-    echo "Not pushing because LOCAL is set"
+else
+    echo "Not pushing because PRODUCTION is not set"
 fi
