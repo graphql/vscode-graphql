ebebde66bd7ad878f56ba295af2dbb2b8d59c87c
Divyendu Singh <singh@prisma.io>
2020-03-12 15:43:32 +0530

    fix script

--- publish.sh
+++ publish.sh
@@ -5,6 +5,14 @@ echo "PAT: $PAT"
 echo "PRODUCTION: $PRODUCTION"
 echo "============================"
 
+if [ "$PRODUCTION" = "1" ]; then
+    git config --global user.email "prismabots@gmail.com"
+    git config --global user.name "Prismo"
+    git remote add github "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
+else
+    echo "Not setting up repo because PRODUCTION is not set"
+fi
+
 if [ -z "$PAT" ]; then
     echo "\$PAT is empty. Please set the value of $PAT"
 elif [ -n "$PAT" ]; then
@@ -17,9 +25,6 @@ elif [ -n "$PAT" ]; then
 fi
 
 if [ "$PRODUCTION" = "1" ]; then
-    git config --global user.email "prismabots@gmail.com"
-    git config --global user.name "Prismo"
-    git remote add github "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
     git pull github "${GITHUB_REF}" --ff-only
     git push github HEAD:"${GITHUB_REF}"
 else
