f761d5ab2c1bf271ecc5cc78bdd6e07a048e9e57
Divyendu Singh <singh@prisma.io>
2020-03-12 14:26:29 +0530

    push to ref

--- publish.sh
+++ publish.sh
@@ -5,6 +5,9 @@ then
   export $(cat .envrc | xargs)
 fi
 
+echo "PAT: $PAT"
+echo "LOCAL: $LOCAL"
+
 if [ -z "$PAT" ]
 then
     echo "\$PAT is empty. Please set the value of $PAT"
@@ -14,9 +17,18 @@ else
         echo "Printing the command because LOCAL is set"
         echo "npm run vsce:publish patch --pat $PAT"
     else
-        npm run vsce:publish patch --pat $PAT
-        git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
-        git pull github "${GITHUB_REF}" --ff-only
-        git push github HEAD:"${GITHUB_REF}"
+        ./node_modules/.bin/vsce publish patch --pat $PAT
     fi
+fi
+
+echo "PAT: $PAT"
+echo "LOCAL: $LOCAL"
+
+if [ -z "$LOCAL" ]
+then
+    echo "Not pushing because LOCAL is set"
+else
+    git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
+    git pull github "${GITHUB_REF}" --ff-only
+    git push github HEAD:"${GITHUB_REF}"
 fi
