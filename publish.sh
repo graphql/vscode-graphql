927b1fbeee3662892a77cfb567a522aa1d608ce6
Divyendu Singh <singh@prisma.io>
2020-03-12 15:05:54 +0530

    fix bash

--- publish.sh
+++ publish.sh
@@ -10,12 +10,12 @@ echo "LOCAL: $LOCAL"
 
 if [[ -z "$PAT" ]]; then
     echo "\$PAT is empty. Please set the value of $PAT"
-else
+elif [[ -n "$PAT" ]]; then
     if [[ -z "$LOCAL" ]]; then
+        ./node_modules/.bin/vsce publish patch --pat $PAT
+    elif [[ -n "$LOCAL" ]]; then
         echo "Printing the command because LOCAL is set"
         echo "npm run vsce:publish patch --pat $PAT"
-    else
-        ./node_modules/.bin/vsce publish patch --pat $PAT
     fi
 fi
 
@@ -23,11 +23,11 @@ echo "PAT: $PAT"
 echo "LOCAL: $LOCAL"
 
 if [[ -z "$LOCAL" ]]; then
-    echo "Not pushing because LOCAL is set"
-else
     git config --global user.email "prismabots@gmail.com"
     git config --global user.name "Prismo"
     git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" || true
     git pull github "${GITHUB_REF}" --ff-only
     git push github HEAD:"${GITHUB_REF}"
+elif [[ -n "$LOCAL" ]]; then
+    echo "Not pushing because LOCAL is set"
 fi
